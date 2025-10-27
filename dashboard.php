62f7ec083cc5bdace76bdff6015f2b27961f1afa
Nyangasi-maker <shabaninyangasi@gmail.com>
2025-10-27 15:01:36 +0300

    about.html

---
+++ dashboard.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dashboard</title>
+</head>
+<body>
+  <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
+  <p>You are now on the dashboard.</p>
+  <a href="logout.php">Logout</a>
+</body>
+</html>
